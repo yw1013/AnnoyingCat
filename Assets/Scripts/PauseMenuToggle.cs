c9903464706d11faeb9d591686bd6c32b54b793b
yw <ywjulie1013@gmail.com>
2018-07-08 19:51:03 -0400

    Game Menu InGame Menu

---
+++ Assets/Scripts/PauseMenuToggle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class PauseMenuToggle : MonoBehaviour {
+
+	private CanvasGroup canvasGroup;
+
+	void Awake () {
+		if (canvasGroup == null) {
+			Debug.LogError ("can't find CanvasGroup ");
+		}
+		canvasGroup = GetComponent<CanvasGroup> ();
+
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.Escape)) {
+			if (canvasGroup.interactable) {
+				Time.timeScale = 1f;
+				canvasGroup.interactable = false; 
+				canvasGroup.blocksRaycasts = false; 
+				canvasGroup.alpha = 0f;
+			} else {
+				Time.timeScale = 0f;
+				canvasGroup.interactable = true; 
+				canvasGroup.blocksRaycasts = true; 
+				canvasGroup.alpha = 1f;
+			} 
+		}
+
+	}
+}
