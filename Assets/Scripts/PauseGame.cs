1a6c2988b251ca0b3c4c59f4e8c98dbfbbd10271
yw <ywjulie1013@gmail.com>
2018-07-08 19:37:39 -0400

    pause game

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour {
+
+	public void gamePause() {
+		Time.timeScale = 0f;
+		Debug.LogError ("Pause Game");
+	}
+
+}
