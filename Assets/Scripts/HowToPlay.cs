54b688fd4be6cffe4c84b295a1ad47b93b44ec2a
yw <ywjulie1013@gmail.com>
2018-07-08 19:13:08 -0400

    Game Menu

---
+++ Assets/Scripts/HowToPlay.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HowToPlay : MonoBehaviour {
+
+	public void Instruction() {
+		Time.timeScale = 0f;
+		Debug.LogError ("How to Play");
+//		SceneManager.LoadScene ("demo");
+	}
+
+}
