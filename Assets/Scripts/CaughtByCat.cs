8ade7a2ece23422b890232fcec98fe9161f4b50d
yw <ywjulie1013@gmail.com>
2018-07-10 18:03:44 -0400

    cat script & animator

---
+++ Assets/Scripts/CaughtByCat.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaughtByCat : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision c)
+    {
+        if (c.gameObject.name == "Player_Cat")
+        {
+            //SomeCatScript catScript = GameObject.GetComponent...
+            //catScript.AddMouseBonusPoint()
+            Destroy(this.gameObject);
+        }
+    }
+}
