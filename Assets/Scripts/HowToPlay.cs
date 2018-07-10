2c1f5eb87b01c8ae4dfe0c58e6b24457fdfbb8a0
yw <ywjulie1013@gmail.com>
2018-07-10 15:52:38 -0400

    How To Play

--- Assets/Scripts/HowToPlay.cs
+++ Assets/Scripts/HowToPlay.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(CanvasGroup))]
 public class HowToPlay : MonoBehaviour {
+
+	private CanvasGroup canvasGroup;
+
+	void Awake () {
+		if (canvasGroup == null) {
+			Debug.LogError ("can't find CanvasGroup ");
+		}
+		canvasGroup = GetComponent<CanvasGroup> ();
+
+	}
 
 	public void Instruction() {
 		Time.timeScale = 0f;
 		Debug.LogError ("How to Play");
-//		SceneManager.LoadScene ("demo");
 	}
-
+		
 }
