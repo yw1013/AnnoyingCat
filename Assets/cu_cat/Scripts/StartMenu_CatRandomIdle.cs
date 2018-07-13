724c382a3c546fa54fea73d81d6e6548d301743e
ycho303 <ycho303@gatech.edu>
2018-07-13 01:31:00 -0400

    GameMenu scene modification
    Added random idle animation to the cat in the start menu by creating a new controller and a script (and slight modification in cu_cat2@B_idle). New BGM to the game as well.

---
+++ Assets/cu_cat/Scripts/StartMenu_CatRandomIdle.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu_CatRandomIdle : MonoBehaviour {
+
+    Animator anim;
+    float randomNumber;
+
+	// Use this for initialization
+	void Start () {
+        anim = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log(randomNumber);
+        randomNumber = Random.Range(0f, 100f);
+         
+        if (0f < randomNumber && randomNumber < 10f) {
+            anim.SetInteger("motion", 10);
+        } 
+
+        else if (20f < randomNumber && randomNumber < 25f) {
+            anim.SetInteger("motion", 20);
+        } 
+
+        else if (70f < randomNumber && randomNumber < 75f) {
+            anim.SetInteger("motion", 30);
+        } 
+
+        else if (90f < randomNumber && randomNumber < 95f) {
+            anim.SetInteger("motion", 40);
+        } 
+
+        else {
+            anim.SetInteger("motion", 50);
+        }
+
+	}
+}
