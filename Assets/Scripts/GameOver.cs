d6379c3d2275e2298f4d9572356cf6c9b9121195
yw <ywjulie1013@gmail.com>
2018-07-16 16:35:04 -0400

    Timer & GameOver

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour {
+
+	public Timer timerManager;
+
+
+	Animator anim;
+
+
+	void Awake() {
+		anim = GetComponent<Animator>();
+	}
+
+
+	void Update() {
+		if (timerManager.timer <= 0) {
+			anim.SetTrigger("GameOver");
+		}
+	}
+}
+
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+	public string levelToLoad;
+	public float timer = 10f;
+	private Text timerSeconds;
+
+	void Start() {
+		timerSeconds = GetComponent<Text> ();
+	}
+
+	void Update() {
+		timer -= Time.deltaTime;
+		timerSeconds.text = timer.ToString ("f2");
+		if (timer <= 0.00) {
+			timerSeconds.text = "0.00";
+		}
+	}
+}
