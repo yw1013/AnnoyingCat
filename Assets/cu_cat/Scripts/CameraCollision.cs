22c2f410340a69697bd7b7e2fe6777040376f4eb
ycho303 <ycho303@gatech.edu>
2018-07-18 19:50:35 -0400

    Cat Camera
    Now the camera orbits around the cat. Also raycasts objects to not see through walls.

---
+++ Assets/cu_cat/Scripts/CameraCollision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollision : MonoBehaviour {
+
+    public float minDis = 0.7f;
+    public float maxDis = 2.5f;
+    public float smooth = 100f;
+    Vector3 dir;
+    public float distance;
+
+	void Awake () {
+        dir = transform.localPosition.normalized;
+        distance = transform.localPosition.magnitude;
+	}
+	
+	void Update () {
+        Vector3 desiredCamPos = transform.parent.TransformPoint(dir * maxDis);
+        RaycastHit hit;
+
+        if (Physics.Linecast (transform.parent.position, desiredCamPos, out hit)) {
+            distance = Mathf.Clamp((hit.distance), minDis, maxDis);
+        } else {
+            distance = maxDis;
+        }
+
+        transform.localPosition = Vector3.Lerp(transform.localScale, dir * distance, Time.deltaTime * smooth);
+	}
+}
---
+++ Assets/cu_cat/Scripts/PlayerFollow.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour {
+
+    public float CameraMoveSpeed = 10f;
+    public GameObject CamFollowObj;
+    public float clampAngle = 8f;
+    public float inputSensitivity = 35f;
+    public float mouseX;
+    public float mouseY;
+    private float rotX = 0f;
+    private float rotY = 0f;
+
+
+	void Start () {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotX = rot.x;
+        rotY = rot.y;
+        Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
+    }
+
+	private void Update()
+	{
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+
+        rotY += mouseX * inputSensitivity * Time.deltaTime;
+        rotX += mouseY * inputSensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0f);
+        transform.rotation = localRotation;
+
+
+	}
+
+	void LateUpdate () {
+        CameraUpdater();
+	}
+
+    void CameraUpdater() {
+        Transform target = CamFollowObj.transform;
+        float step = CameraMoveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
