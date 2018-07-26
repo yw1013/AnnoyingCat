9b9286f6cdcc6a2eae092f607d05d36886ca9414
yw <ywjulie1013@gmail.com>
2018-07-25 23:06:04 -0400

    tutorial cursor

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -10,13 +10,18 @@ public class Tutorial : MonoBehaviour {
 
 	void Awake() {
 		anim = GetComponent<Animator>();
+		Cursor.visible = true;
+	}
+
+	void Start() {
+		Cursor.lockState = CursorLockMode.None;
+		Cursor.visible = true;
 	}
 		
 	void Update() {
 		if (timerManager.timer <= 0) {
 			anim.SetTrigger("GameOver");
-			Cursor.lockState = CursorLockMode.None;
-			Cursor.visible = true;
+
 			GameObject.Find("Player_Cat").GetComponent<CatControl>().enabled = false;
 			GameObject.Find("Player_Cat").GetComponent<Animator>().enabled = false;
 		}
