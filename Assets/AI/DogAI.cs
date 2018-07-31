2bff10441f6b346e14bcf94b200f048b3f0d93e8
Sayem Hoque <sayemhoque@Sayems-MacBook-Pro.local>
2018-07-31 08:27:47 -0400

    dog chasing cat script

--- Assets/AI/DogAI.cs
+++ Assets/AI/DogAI.cs
@@ -1,16 +1,53 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DogAI : MonoBehaviour {
+	Animator anim;
+	public Transform Player;
+	int MaxDist = 10;
+	int MinDist = 3;
+	int MoveSpeed = 4;
+
+	bool hasSeenCat = false;
+	bool hasAttacked = false;
 
 	// Use this for initialization
 	void Start () {
-		
+		anim = GetComponent<Animator>();
+		anim.SetFloat("Speed", 0f);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		transform.LookAt(Player);
+
+		if (!hasAttacked && !hasSeenCat && Vector3.Distance(transform.position, Player.position) >= 3)
+		{
+			hasSeenCat = true;
+		}
+
+		if (!hasAttacked && hasSeenCat && (Vector3.Distance(transform.position, Player.position) <= 5) && (Vector3.Distance(transform.position, Player.position) >= 3)) {
+			anim.SetFloat("Speed", 1f);
+			transform.position += transform.forward * MoveSpeed / 4 * Time.deltaTime;
+		}
+
+		if (!hasAttacked && hasSeenCat && Vector3.Distance(transform.position, Player.position) <= 3 && Vector3.Distance(transform.position, Player.position) >= 0.3) {
+			anim.SetFloat("Speed", 3f);
+			transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+		}
+
+		if (!hasAttacked && hasSeenCat && Vector3.Distance(transform.position, Player.position) < 0.3) {
+			transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+			anim.SetTrigger("Jump");
+			hasAttacked = true;
+		}
+
+		if (hasAttacked) {
+			 anim.SetFloat("Speed", 0f);
+		}
 	}
+
+
 }
---
+++ Assets/ChaseCat.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChaseCat : MonoBehaviour {
+	public Transform Player;
+	public Animator anim;
+	float MoveSpeed = 4f;
+	float MaxDist = 10f;
+	float MinDist = 1f;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+			transform.LookAt(Player);
+
+			anim.SetBool("Bark", true);
+
+			// if (Vector3.Distance(transform.position, Player.position) >= MinDist)
+			// {
+			//
+			// 		anim.SetFloat("Speed", MoveSpeed * Time.deltaTime);
+			// 		transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+			//
+			//
+			//
+			// 		if (Vector3.Distance(transform.position, Player.position) <= MaxDist)
+			// 		{
+			// 				//Here Call any function U want Like Shoot at here or something
+			// 		}
+			//
+			// }
+		}
+
+}
